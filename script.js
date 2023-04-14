fb0b8df89733ce091ecc5eb0b1b1aac4ad205209
kaia <89624646+kaiakm@users.noreply.github.com>
2023-04-13 20:35:12 -0500

    computerchoice now works git status hljfhgf hglfjhg cd

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 console.log('Hello World!');
 
-function getComputerChoice(num) {
-    return Math.floor(Math.random() * 3) + 1;
+function getComputerChoice() {
+    result = Math.floor(Math.random() * 3) + 1;
+    
+    if (result === 1) {
+        console.log('rock');
+    } else if (result === 2) {
+        console.log('paper');
+    } else if (result === 3) {
+        console.log('scissors');
+    } else {
+
+    }
 }
 
-console.log(getComputerChoice());
+getComputerChoice();
