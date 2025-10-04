2d76e72630c467957fc10788ca13d3b7a45d2597
kaiakm <89624646+kaiakm@users.noreply.github.com>
2025-10-04 00:37:13 -0500

    added playround function

--- script.js
+++ script.js
@@ -26,3 +26,26 @@ function getHumanChoice () {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound (getHumanChoice, getComputerChoice) {
+    if (getHumanChoice === 'paper' && getComputerChoice ==='rock') {
+        console.log('You win! Paper beats rock.');
+    } else if (getHumanChoice === 'paper' && getComputerChoice === 'scissors') {
+        console.log('You lose! Scissors beats paper.');
+    } else if (getHumanChoice === 'rock' && getComputerChoice === 'paper') {
+        console.log('You lose! Paper beats rock.');
+    } else if (getHumanChoice === 'rock' && getComputerChoice === 'scissors') {
+        console.log('You win! Rock beats scissors.');
+    } else if (getHumanChoice === 'scissors' && getComputerChoice === 'paper') {
+        console.log('You win! Scissors beats paper.');
+    } else if (getHumanChoice === 'scissors' && getComputerChoice === 'rock') {
+        console.log('You lose! Rock beats scissors.');
+    } else {
+        console.log('It`s a tie!');
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
